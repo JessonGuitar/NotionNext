01f2a821682657af05594c4e0fbe0f4fa49fba59
tangly1024 <mail@tangly1024.com>
2026-06-02 15:41:48 +0800

    Fix music widget config syntax

--- conf/widget.config.js
+++ conf/widget.config.js
@@ -16,51 +16,50 @@ module.exports = {
   SPOILER_TEXT_TAG: process.env.NEXT_PUBLIC_SPOILER_TEXT_TAG || '', // Spoiler文本隐藏功能，如Notion中 [sp]希望被spoiler的文字[sp]，填入[sp] 即可
 
   // 音乐播放插件
-MUSIC_PLAYER: process.env.NEXT_PUBLIC_MUSIC_PLAYER || true, // 是否使用音乐播放插件
-MUSIC_PLAYER_VISIBLE:
-  process.env.NEXT_PUBLIC_MUSIC_PLAYER_VISIBLE || true, // 是否在左下角显示播放和切换，如果使用播放器，打开自动播放再隐藏，就会以类似背景音乐的方式播放，无法取消和暂停
-MUSIC_PLAYER_AUTO_PLAY: 
-  process.env.NEXT_PUBLIC_MUSIC_PLAYER_AUTO_PLAY || true, // 是否自动播放，不过自动播放时常不生效（移动设备不支持自动播放）
-MUSIC_PLAYER_LRC_TYPE: 
-  process.env.NEXT_PUBLIC_MUSIC_PLAYER_LRC_TYPE || '0', // 歌词显示类型，可选值： 3 | 1 | 0（0：禁用 lrc 歌词，1：lrc 格式的字符串，3：lrc 文件 url）（前提是有配置歌词路径，对 meting 无效）
-MUSIC_PLAYER_CDN_URL: 
-  process.env.NEXT_PUBLIC_MUSIC_PLAYER_CDN_URL || 'https://cdn.jsdelivr.net/npm/aplayer@1.10.0/dist/APlayer.min.js',
-MUSIC_PLAYER_ORDER:
-  process.env.NEXT_PUBLIC_MUSIC_PLAYER_ORDER || 'list', // 默认播放方式，顺序 list，随机 random
-MUSIC_PLAYER_AUDIO_LIST: 
-  [
-  // 示例音乐列表。除了以下配置外，还可配置歌词，具体配置项看此文档：https://aplayer.js.org/#/zh-Hans/
-  {
-    name: '航行 Voyage',
-    artist: '杨家胜 | Jesson Yang',
-    url: 'https://cdn.jsdelivr.net/gh/JessonGuitar/mp3-cdn@main/%E8%A1%8C%E8%88%AA%20Voyage.mp3',
-    cover: ''
-  },
-  {
-    name: '处女座幻想 Virgo Fantasy',
-    artist: '杨家胜 | Jesson Yang',
-    url: 'https://cdn.jsdelivr.net/gh/JessonGuitar/mp3-cdn@main/%E5%A4%84%E5%A5%B3%E5%BA%A7%E5%B9%BB%E6%83%B3%20Virgo%20Fantasy.mp3',
-    cover: ''
-  },
-  {
-    name: '伴你左右 beside you',
-    artist: '杨家胜 | Jesson Yang',
-    url: 'https://cdn.jsdelivr.net/gh/JessonGuitar/mp3-cdn@main/%E4%BC%B4%E4%BD%A0%E5%B7%A6%E5%8F%B3%20beside%20you.mp3',
-    cover: ''
-  },
-  {
-    name: '喝高的牛仔 Drunk Cowboy',
-    artist: '杨家胜 | Jesson Yang',
-    url: 'https://cdn.jsdelivr.net/gh/JessonGuitar/mp3-cdn@main/%E5%96%9D%E9%AB%98%E7%9A%84%E7%89%9B%E4%BB%94%20Drunk%20Cowboy.mp3',
-    cover: ''
-  },
-  {
-    name: '夏末圆舞曲 Summer Waltz',
-    artist: '杨家胜 | Jesson Yang',
-    url: 'https://cdn.jsdelivr.net/gh/JessonGuitar/mp3-cdn@main/%E5%A4%8F%E6%9C%AB%E5%9C%86%E8%88%9E%E6%9B%B2%20Summer%20Waltz.mp3',
-    cover: ''
-  }
-]
+  MUSIC_PLAYER: process.env.NEXT_PUBLIC_MUSIC_PLAYER || true, // 是否使用音乐播放插件
+  MUSIC_PLAYER_VISIBLE:
+    process.env.NEXT_PUBLIC_MUSIC_PLAYER_VISIBLE || true, // 是否在左下角显示播放和切换，如果使用播放器，打开自动播放再隐藏，就会以类似背景音乐的方式播放，无法取消和暂停
+  MUSIC_PLAYER_AUTO_PLAY:
+    process.env.NEXT_PUBLIC_MUSIC_PLAYER_AUTO_PLAY || true, // 是否自动播放，不过自动播放时常不生效（移动设备不支持自动播放）
+  MUSIC_PLAYER_LRC_TYPE:
+    process.env.NEXT_PUBLIC_MUSIC_PLAYER_LRC_TYPE || '0', // 歌词显示类型，可选值： 3 | 1 | 0（0：禁用 lrc 歌词，1：lrc 格式的字符串，3：lrc 文件 url）（前提是有配置歌词路径，对 meting 无效）
+  MUSIC_PLAYER_CDN_URL:
+    process.env.NEXT_PUBLIC_MUSIC_PLAYER_CDN_URL ||
+    'https://cdn.jsdelivr.net/npm/aplayer@1.10.0/dist/APlayer.min.js',
+  MUSIC_PLAYER_ORDER: process.env.NEXT_PUBLIC_MUSIC_PLAYER_ORDER || 'list', // 默认播放方式，顺序 list，随机 random
+  MUSIC_PLAYER_AUDIO_LIST: [
+    // 示例音乐列表。除了以下配置外，还可配置歌词，具体配置项看此文档：https://aplayer.js.org/#/zh-Hans/
+    {
+      name: '航行 Voyage',
+      artist: '杨家胜 | Jesson Yang',
+      url: 'https://cdn.jsdelivr.net/gh/JessonGuitar/mp3-cdn@main/%E8%A1%8C%E8%88%AA%20Voyage.mp3',
+      cover: ''
+    },
+    {
+      name: '处女座幻想 Virgo Fantasy',
+      artist: '杨家胜 | Jesson Yang',
+      url: 'https://cdn.jsdelivr.net/gh/JessonGuitar/mp3-cdn@main/%E5%A4%84%E5%A5%B3%E5%BA%A7%E5%B9%BB%E6%83%B3%20Virgo%20Fantasy.mp3',
+      cover: ''
+    },
+    {
+      name: '伴你左右 beside you',
+      artist: '杨家胜 | Jesson Yang',
+      url: 'https://cdn.jsdelivr.net/gh/JessonGuitar/mp3-cdn@main/%E4%BC%B4%E4%BD%A0%E5%B7%A6%E5%8F%B3%20beside%20you.mp3',
+      cover: ''
+    },
+    {
+      name: '喝高的牛仔 Drunk Cowboy',
+      artist: '杨家胜 | Jesson Yang',
+      url: 'https://cdn.jsdelivr.net/gh/JessonGuitar/mp3-cdn@main/%E5%96%9D%E9%AB%98%E7%9A%84%E7%89%9B%E4%BB%94%20Drunk%20Cowboy.mp3',
+      cover: ''
+    },
+    {
+      name: '夏末圆舞曲 Summer Waltz',
+      artist: '杨家胜 | Jesson Yang',
+      url: 'https://cdn.jsdelivr.net/gh/JessonGuitar/mp3-cdn@main/%E5%A4%8F%E6%9C%AB%E5%9C%86%E8%88%9E%E6%9B%B2%20Summer%20Waltz.mp3',
+      cover: ''
+    }
+  ],
   MUSIC_PLAYER_METING: process.env.NEXT_PUBLIC_MUSIC_PLAYER_METING || false, // 是否要开启 MetingJS，从平台获取歌单。会覆盖自定义的 MUSIC_PLAYER_AUDIO_LIST，更多配置信息：https://github.com/metowolf/MetingJS
   MUSIC_PLAYER_METING_SERVER:
     process.env.NEXT_PUBLIC_MUSIC_PLAYER_METING_SERVER || 'netease', // 音乐平台，[netease, tencent, kugou, xiami, baidu]
